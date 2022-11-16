50e87b9c7078552144547fd6d3a7647369b89256
sarah jallon <sarah.jallon@gmail.com>
2022-11-16 15:50:24 +0100

    package and setup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import hanoi.gui.JHanoi;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        JHanoi j = new JHanoi();
     }
 }
